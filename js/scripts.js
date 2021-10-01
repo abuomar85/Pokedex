5f60daa3c2ffda1f6b5fdbcf8ec99e2174010aa8
Abdullah Alyabroude <abu-omar.852@hotmail.com>
2021-10-01 10:33:38 +0200

    update js file and delete the old code
    delete code from css and html because it is no longer needed.
    update js file by adding an array with three pokemon object

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello world');
-let favoriteFood  = 'checken'; 
-document.write(favoriteFood);
+// Creating Array with three pokemon all pokemon detail from https://pokedex.org/
+let pokemonList = [
+                    {name: 'Bulbasaur',height: 7, types: ['grass', 'poison'] },
+                    {name: 'Ivysaur',height: 10, types: ['grass', 'poison']  },
+                    {name: 'Venusaur',height: 20, types: ['grass', 'poison']  }
+                  ];
