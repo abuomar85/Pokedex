329eb215d73aef9c3b258437b48e6c992ef22be0
Abdullah Alyabroude <abu-omar.852@hotmail.com>
2021-10-05 17:40:56 +0200

    update js file adding for loop and one condition
    looping throgh array and type the name and height for every pokemon and make a condition to check if any pokemon is equal or greater than 20

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,11 @@ let pokemonList = [
                     {name: 'Ivysaur',height: 10, types: ['grass', 'poison']  },
                     {name: 'Venusaur',height: 20, types: ['grass', 'poison']  }
                   ];
+                  //looping through pokemon array and show the names an height
+for (var i = 0; i < pokemonList.length; i++) {
+document.write(pokemonList[i].name + " (height: "  + pokemonList[i].height + ") " + "<br>"  );
+//make a condition to check if pokemon is equal or greater than 20
+if(pokemonList[i].height >= 20) {
+  document.write(pokemonList[i].name + " is realy big");
+}
+}
