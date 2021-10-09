ea82bf7f45577b43f2ac5ec52e62afdd8dd5d60a
Abdullah Alyabroude <abu-omar.852@hotmail.com>
2021-10-09 13:56:05 +0200

    updating js file to IIFE
    wrap pokemon list array in an IIFE to avoid accidentally accessing the global state.
    create a new pokemonRepository variable to hold what IIFE will return, then assign the IIFE to that variable.
    The IIFE return an object with the following public functions assigned as keys:
    getAll
    add
     functions are defined separately with the function keyword
    update the loop code to cope with the new changes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,43 @@
   // Creating Array with three pokemon all pokemon detail from https://pokedex.org/
   // wraping pokemonList array in an IIFE to avoid accidentally accessing the global state. creating pokeminRepository
-  let pokemonList = [
-                      {name: 'Bulbasaur',height: 7, types: ['grass', 'poison'] },
-                      {name: 'Ivysaur',height: 10, types: ['grass', 'poison']  },
-                      {name: 'Venusaur',height: 20, types: ['grass', 'poison']  }
-                    ];
+  let pokemonRepository = (function () {
+    let pokemonList = [
+                        {name: 'Bulbasaur',height: 7, types: ['grass', 'poison'] },
+                        {name: 'Ivysaur',height: 10, types: ['grass', 'poison']  },
+                        {name: 'Venusaur',height: 20, types: ['grass', 'poison']  }
+                      ];
+    // return the pokemonList array
+    function getAll() {
+      return pokemonList;
+    }
+    //adding new pokemon to the array
+    function add(pokemon) {
+      
+      return pokemonList.push(pokemon);
+    }
+    // return the new array with new items
+    return {
+      add: add,
+      getAll, getAll
+    };
+  })();
 
                   //looping through pokemon array and show the names an height
   document.write("<h3>Foor loop</h3>")
-  for (var i = 0; i < pokemonList.length; i++) {
-  document.write(pokemonList[i].name + " (height: "  + pokemonList[i].height + ") " + "<br>"  );
+  for (var i = 0; i < pokemonRepository.getAll().length; i++) {
+  document.write(pokemonRepository.getAll()[i].name + " (height: "  + pokemonRepository.getAll()[i].height + ") " + "<br>"  );
   //make a condition to check if pokemon is equal or greater than 20
-  if(pokemonList[i].height >= 20) {
-    document.write(pokemonList[i].name + " is realy big");
+  if(pokemonRepository.getAll()[i].height >= 20) {
+    document.write(pokemonRepository.getAll()[i].name + " is realy big");
   }
   }
   //looping through array using ForEach
   document.write("<h3>ForEach loop</h3>")
-  pokemonList.forEach(function(item){
+  pokemonRepository.getAll().forEach(function(item){
     document.write(item.name + " (height: "  + item.height + ") " + "<br>"  );
+    if(item.height >= 20 ) {
+      document.write(item.name + " is realy big");
+    }
+
+
   })
